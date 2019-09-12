f56747e962c4970f4d59f5b88d6f890bdbcd9bb5
ben10debian <https://github.com/Ben-HurFigueiredo/CursoAndroid_ControleFinanceiro.git>
2019-09-11 22:06:28 -0300

    comitando 6

--- app/src/main/java/br/com/basicsistemas/controlefinanceiro/Principal.java
+++ app/src/main/java/br/com/basicsistemas/controlefinanceiro/Principal.java
@@ -1,29 +1,16 @@
 package br.com.basicsistemas.controlefinanceiro;
 
-import android.content.Intent;
+
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import br.com.basicsistemas.controlefinanceiro.views.Produto;
-import br.com.basicsistemas.controlefinanceiro.views.Produtos;
+
 
 public class Principal extends MenuPrincipal {
 
 
-    TextView textoPrincipal;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +20,7 @@ public class Principal extends MenuPrincipal {
         setSupportActionBar(toolbar);
 
 
-       textoPrincipal.setText(R.string.lb_campo_ola_mundo);
+       //textoPrincipal.setText(R.string.lb_campo_ola_mundo);
 
     }
 
